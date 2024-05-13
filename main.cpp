3c8a76ddc5a212ad534946e26ef27c6adda34f00
Dmytro <ddvornikov0309gmail.com>
2024-05-13 14:50:48 +0200

    Додано вивід від різниці введених чисел

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
     int a = 0, b = 0;
     cout<<"Please, enter the numbers a and b ";
     cin>>a>>b;
-    cout<<"Sum of entered numbers is "<<(a + b)<<endl;
+    cout<<"Difference of entered numbers is "<<(a - b)<<endl;
 
     return 0;
 }
