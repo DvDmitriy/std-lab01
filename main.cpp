57c655643f4521709039193b732783954edd8a28
Dmytro <ddvornikov0309gmail.com>
2024-05-13 16:48:52 +0200

    Додано множення

--- main.cpp
+++ main.cpp
@@ -2,13 +2,14 @@
 
 using namespace std;
 
-//Програма э навчальною
 int main()
 {
     int a = 0, b = 0;
     cout<<"Please, enter the numbers a and b ";
     cin>>a>>b;
-    cout<<"Difference of entered numbers is "<<(a - b)<<endl;
+    cout<<"a + b ="<< a + b << endl;
+        <<"a - b ="<< a - b << endl;
+        <<"a * b ="<< a * b << endl;
 
     return 0;
 }
